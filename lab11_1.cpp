f5015694ed2ae42b554111cf9e8d3cd4a01a89d5
ktzboy <pongpol.sritalanut@gmail.com>
2024-12-20 16:22:24 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,46 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string random() {
+    int i = rand() % 9;
+    switch(i) {
+        case 0 :
+            return "A";
+            break;
+        case 1 :
+            return "B+";
+            break;
+        case 2 :
+            return "B";
+            break;
+        case 3 :
+            return "C+";
+            break;
+        case 4 :
+            return "C";
+            break;
+        case 5 :
+            return "D+";
+            break;
+        case 6 :
+            return "D";;
+            break;
+        case 7 :
+            return "F";;
+            break;
+        case 8 :
+            return "W";;
+            break;
+    }
+}
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    cout << "You will get " << random() << " in this 261102.";
+}
